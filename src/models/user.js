8929006497393b4231d1e7f660ed87076e44cb39
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-04-02 00:25:33 -0300

    Completing user model

--- src/models/user.js
+++ src/models/user.js
@@ -1,9 +1,36 @@
+import bcrypt from 'bcryptjs';
 import mongoose from 'mongoose';
+import validator from 'validator';
+
+const { isEmail } = validator;
 
 const { Schema } = mongoose;
+const SALT_WORK_FACTOR = 10;
 
-const userSchema = new Schema({
-  username: String,
-  createdDate: { type: Date, default: Date.now },
+const UserSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    validate: [isEmail],
+  },
+  password: { type: String, required: true },
 });
-export default mongoose.model('User', userSchema);
+
+UserSchema.pre('save', async function save(next) {
+  if (!this.isModified('password')) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+UserSchema.index({ email: 1 }, { unique: true });
+
+UserSchema.methods.validatePassword = async function validatePassword(data) {
+  return bcrypt.compare(data, this.password);
+};
+
+export default mongoose.model('User', UserSchema);
