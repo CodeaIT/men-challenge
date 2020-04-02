c72d0bc5c0911af878a0cf541635a1c6fcc0a351
Maximiliano Ejberowicz <maxiejbe92@gmail.com>
2020-04-02 00:22:40 -0300

    Singular names for routes and test

--- src/routes/user.js
+++ src/routes/user.js
@@ -2,8 +2,10 @@ import express from 'express';
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-  res.send('respond with a resource');
-});
+router.get('/', [
+  (req, res) => {
+    res.send('respond with a resource');
+  },
+]);
 
 export default router;
